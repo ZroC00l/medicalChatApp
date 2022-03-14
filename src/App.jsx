b6f4c80e4b9d329f41ef67e83c87c0aefa0504bd
ZroC00l <giftgabanakgosi@gmail.com>
2022-03-14 09:54:57 +0200

    active channels layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
 import { ChannelListContainer, ChannelContainer, Auth } from "./components";
+import "../node_modules/stream-chat-react/dist/css/index.css";
 import "./App.css";
 
 const apiKey = "6fzc8pxn87t9";
@@ -29,14 +30,30 @@ if (authToken) {
 }
 
 const App = () => {
+  //create state to listen to the channel that are active within channelcontainer as well as channelListContainer
+  const [createType, setCreateType] = useState("");
+  const [isCreating, setIsCreating] = useState(false);
+  const [isEditing, setIsEditing] = useState(false);
+
   /*IF the user is not authenticated they shouldnt go past the Authentication */
   if (!authToken) return <Auth />;
 
   return (
     <div className="app__wrapper">
       <Chat client={client} theme="team light">
-        <ChannelListContainer />
-        <ChannelContainer />
+        <ChannelListContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+        <ChannelContainer
+          isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+          createType={createType}
+        />
       </Chat>
     </div>
   );
